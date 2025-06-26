1e57d143b008f075a4e1fe6c4fbc783a18da3b6d
AbhishekChauhanNITRR <154234725+AbhishekChauhanNITRR@users.noreply.github.com>
2025-06-26 19:34:04 +0530

    Update emailVerificationTemplate.js

--- Server/mailTemplates/emailVerificationTemplate.js
+++ Server/mailTemplates/emailVerificationTemplate.js
@@ -66,8 +66,6 @@ const otpTemplate = (otp) => {
 	
 	<body>
 		<div class="container">
-			<a href="https://virtual_guruji-edtech-project.vercel.app"><img class="logo"
-					src="https://i.ibb.co/7Xyj3PC/logo.png" alt="virtual_guruji Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
