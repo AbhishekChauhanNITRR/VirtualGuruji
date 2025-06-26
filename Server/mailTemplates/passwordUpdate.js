53996c49bb0a039414a7bec003d892486cdf00bc
AbhishekChauhanNITRR <154234725+AbhishekChauhanNITRR@users.noreply.github.com>
2025-06-26 19:34:44 +0530

    Update passwordUpdate.js

--- Server/mailTemplates/passwordUpdate.js
+++ Server/mailTemplates/passwordUpdate.js
@@ -55,8 +55,6 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://virtual_guruji-edtech-project.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="virtual_guruji Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
