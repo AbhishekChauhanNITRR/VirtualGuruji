bff6f16ab6083b002a1a2bd215b94a1aa088ac1a
AbhishekChauhanNITRR <154234725+AbhishekChauhanNITRR@users.noreply.github.com>
2025-06-26 19:33:38 +0530

    Update courseEnrollmentEmail.js

--- Server/mailTemplates/courseEnrollmentEmail.js
+++ Server/mailTemplates/courseEnrollmentEmail.js
@@ -67,8 +67,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
     
     <body>
         <div class="container">
-            <a href="https://virtual_guruji-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
-                    alt="virtual_guruji Logo"></a>
+
             <div class="message">Course Registration Confirmation</div>
             <div class="body">
                 <p>Dear ${name},</p>
@@ -76,7 +75,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
                     are excited to have you as a participant!</p>
                 <p>Please log in to your learning dashboard to access the course materials and start your learning journey.
                 </p>
-                <a class="cta" href="https://virtual_guruji-edtech-project.vercel.app/dashboard">Go to Dashboard</a>
+               
             </div>
             <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
                     href="mailto:info@virtual_guruji.com">info@virtual_guruji.com</a>. We are here to help!</div>
